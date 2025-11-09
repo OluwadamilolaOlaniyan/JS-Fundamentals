60efafa621e0c425205a22901b71933ef53820e2
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2025-11-09 20:48:17 +0100

    update

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
-const numberOfArguments = ProcessingInstruction.argv.length - 2;
+const numberOfArguments = process.argv.length;
 
 if (numberOfArguments === 0) {
     console.log("No argument");
 } else if (numberOfArguments === 1) {
     console.log("Argument found");
 } else {
-    console.log("Argument found");
+    console.log("Arguments found");
 }
